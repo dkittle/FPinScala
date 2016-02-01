004e8ce120af47acbf0d21fc25f95e2d4b062b11
dkittle <don@kittle.ca>
2016-02-01 17:48:30 -0500

    adding solution for variance

--- src/main/scala/chapter4/Option.scala
+++ src/main/scala/chapter4/Option.scala
@@ -1,13 +1,29 @@
 package chapter4
 
-import scala.{Option => _, Either => _, _}
+import scala.{Some => _, None => _, Option => _, Either => _, _}
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = ???
-  def flatMap[B](f: A => Option[B]) : Option[B] = ???
-  def getOrElse[B >: A](default: => B): B = ???
+
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(x) => Some(f(x))
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match{
+    case None => default
+    case Some(x) => x
+  }
+
+  def flatMap[B](f: A => Option[B]) : Option[B] = {
+    map(f).getOrElse(None)
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(x) if f(x) => this
+    case _ => None
+  }
+
   def orElse[B >: A](ob: => Option[B]): Option[B] = ???
-  def filter(f: A => Boolean): Option[A] = ???
 
 }
 
--- src/test/scala/chapter4/OptionSpec.scala
+++ src/test/scala/chapter4/OptionSpec.scala
@@ -1,17 +1,55 @@
 package chapter4
 
-import scala.{Option => _, Either => _, _}
+import scala.{Some => _, None => _, Option => _, Either => _, _}
 
 import org.scalatest.{Matchers, FlatSpec}
 import chapter4.{Option, None, Some }
 
 class OptionSpec extends FlatSpec with Matchers {
 
-  "A mean" should "return an Option[Double]" in {
-    def mean(xs: Seq[Double]): Option[Double] = {
-      if (xs.isEmpty) None
-      else Some(xs.sum / xs.length)
+  def mean(xs: Seq[Double]): Option[Double] = {
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+  }
+
+  def variance(xs: Seq[Double]): Option[Double] = {
+    if (xs.isEmpty) None
+    else {
+      val m = mean(xs).getOrElse(1.0)
+      val v = xs.map(x => math.pow(x - m, 2))
+      mean(v)
     }
   }
 
+  "A mean" should "return an Some(5.0)" in {
+    mean(Seq(2.0, 5.0, 8.0)) should be (Some(5.0))
+  }
+
+  "A mean" should "return a None" in {
+    mean(Seq()) should be (None)
+  }
+
+  "An Option" should "return a string with getOrElse" in {
+    val foo: Option[String] = Some("foo")
+    foo.getOrElse("") should be ("foo")
+  }
+
+  "An Option" should "return None with getOrElse" in {
+    val foo: Option[String] = None
+    foo.getOrElse(None) should be (None)
+  }
+
+  "An Option of Some" should "return foo with map" in {
+    val foo: Option[String] = Some("foo")
+    foo.map(_.toString).getOrElse(None) should be ("foo")
+  }
+
+  "An Option of Some" should "return foo with filter" in {
+    val foo: Option[String] = Some("foo")
+    foo.filter(_ == "foo").getOrElse(None) should be ("foo")
+  }
+
+  "A seq of doubles 2, 5, 8" should "have a variance of 6" in {
+    variance(Seq(2.0, 5.0, 8.0)) should be (Some(6.0))
+  }
 }
