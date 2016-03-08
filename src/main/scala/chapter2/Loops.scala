3a154200f5e9b45e1ed73b294e1839e905aff76b
dkittle <don@kittle.ca>
2016-03-08 10:20:52 -0500

    added fibonacci function

--- src/main/scala/chapter2/Loops.scala
+++ src/main/scala/chapter2/Loops.scala
@@ -12,5 +12,15 @@ object Loops {
     rec(n, 1)
   }
 
+  def fibonacci(n:Int): Int = {
+    @tailrec
+    def rec(x: Int, p1: Int, p2: Int): Int = {
+      if (x <= 1) 0 + p1 else
+        rec(x - 1, p2 + p1, p1)
+    }
+    if (n <= 1) 0 else
+      rec(n - 1, 1, 0)
+  }
+
 
 }
--- src/test/scala/LoopsSpec.scala
+++ src/test/scala/LoopsSpec.scala
@@ -17,4 +17,27 @@ class LoopsSpec extends FlatSpec with Matchers {
     factorial(1) should be (1)
   }
 
+  "fibonacci(1)" should "return 0" in {
+    fibonacci(1) should be (0)
+  }
+
+  "fibonacci(2)" should "return 1" in {
+    fibonacci(2) should be (1)
+  }
+
+  "fibonacci(3)" should "return 1" in {
+    fibonacci(3) should be (1)
+  }
+
+  "fibonacci(4)" should "return 2" in {
+    fibonacci(4) should be (2)
+  }
+
+  "fibonacci(5)" should "return 3" in {
+    fibonacci(5) should be (3)
+  }
+
+  "fibonacci(6)" should "return 5" in {
+    fibonacci(6) should be (5)
+  }
 }
