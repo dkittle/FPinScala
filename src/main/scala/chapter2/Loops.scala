d52e45f10c0d02cba25339e1cd4ff05e21335b43
dkittle <don@kittle.ca>
2016-03-08 08:18:32 -0500

    added tailrec annotation

--- src/main/scala/chapter2/Loops.scala
+++ src/main/scala/chapter2/Loops.scala
@@ -1,8 +1,11 @@
 package chapter2
 
+import scala.annotation.tailrec
+
 object Loops {
 
   def factorial(n: Int): Int = {
+    @tailrec
     def rec(x: Int, acc: Int): Int =
       if(x <= 0) acc
       else rec(x-1, acc * x)
