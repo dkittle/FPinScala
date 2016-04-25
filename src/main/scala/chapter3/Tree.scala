c36b8ecd7c5703f2c9f40ab62f4c3940073f0277
dkittle <don@kittle.ca>
2016-04-25 18:00:26 -0400

    added Tree impl

---
+++ src/main/scala/chapter3/Tree.scala
@@ -0,0 +1,31 @@
+package chapter3
+
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l, r) => size(l) + 1 + size(r)
+  }
+
+  def highestValue(t: Tree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l, r) => highestValue(l) max highestValue(r)
+  }
+
+  def sum(t: Tree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l, r) => sum(l) + sum(r)
+  }
+
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case (Branch(l, r)) => (depth(l) + 1) max (depth(r) + 1)
+  }
+
+
+}
---
+++ src/test/scala/chapter3/TreeSpec.scala
@@ -0,0 +1,58 @@
+package chapter3
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class TreeSpec extends FlatSpec with Matchers {
+
+
+  "A tree" should "have size 3" in {
+    val f = Branch(Leaf(1), Leaf(2))
+    Tree.size(f) should be (3)
+  }
+
+  it should "have size 1" in {
+    val f = Leaf(99)
+    Tree.size(f) should be (1)
+  }
+
+  it should "size 7" in {
+    val f = Branch(Branch(Leaf(0), Leaf(123)), Branch(Leaf(2), Leaf(4)))
+    Tree.size(f) should be (7)
+  }
+
+  it should "sum to 12" in {
+    val f = Leaf(12)
+    Tree.sum(f) should be (12)
+  }
+
+  it should "sum to 5" in {
+    val f = Branch(Leaf(2), Leaf(3))
+    Tree.sum(f) should be (5)
+  }
+
+  it should "sum to 129" in {
+    val f =   Branch(Branch(Leaf(0), Leaf(123)), Branch(Leaf(2), Leaf(4)))
+    Tree.sum(f) should be (129)
+  }
+
+  it should "have a depth of 1" in {
+    val f = Leaf(13)
+    Tree.depth(f) should be (1)
+  }
+
+  it should "have a depth of 3" in {
+    val f =   Branch(Leaf(43), Branch(Leaf(1), Leaf(2)))
+    Tree.depth(f) should be (3)
+  }
+
+  it should "have a depth of 4" in {
+    val f = Branch(Branch(Leaf(1), Branch(Leaf(2), Leaf(3))), Branch(Leaf(14), Leaf(15)))
+    Tree.depth(f) should be (4)
+  }
+
+  it should "have a hightest value 14" in {
+    val f = Branch(Branch(Leaf(1), Branch(Leaf(2), Leaf(3))), Branch(Leaf(14), Leaf(15)))
+    Tree.highestValue(f) should be (15)
+  }
+
+}
